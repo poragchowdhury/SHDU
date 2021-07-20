450b1b141629186a51e8c916ae6dc4e230811ff6
Moinul Morshed Porag Chowdhury <poragbuet@gmail.com>
2021-07-19 19:17:39 -0600

    small edits

--- src/SHDU.java
+++ src/SHDU.java
@@ -160,9 +160,9 @@ public class SHDU {
 			// call the simulate training data to generate training dataset
 			// House.SEED = 2;
 			// house.simulateTrainingData("test_set_1_1_small");
-		House.SEED = 1;
+		House.SEED = 2;
 		House house = new House(HOUSE_SIZE.SMALL.ordinal(), "inputs/random2/Set_1_1.json");
-		house.simulateTrainingData("SM_set_1_1");
+		house.simulateTrainingData("SM_set_1_1_testdata");
 		// call the generateSchedules method to predict the schedules of the devices		
 		//house.generateSchedules();
 	}
